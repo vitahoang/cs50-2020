f4827205f9869a92af552c02f86b8fe0f2852dd7
VitaHoang <hvhung.oc@gmail.com>
2023-06-19 14:34:54 +0700

    refactor(pylint)

--- final/control.py
+++ final/control.py
@@ -1,3 +1,4 @@
+"""Provide Methods to control"""
 import time
 
 import cv2
@@ -8,6 +9,7 @@ from utils import process_running
 
 
 def click(x, y):
+    """click to point(x,y) with sleep"""
     pyautogui.moveTo(x, y)
     time.sleep(2)
     pyautogui.click()
@@ -15,26 +17,27 @@ def click(x, y):
 
 
 def open_app():
-    # Open MUAway app
+    """open MUAway app"""
     pyautogui.click()
     pyautogui.hotkey("command", "space")
     pyautogui.write(["m", "u", "a", "w", "a", "y"])
     pyautogui.press('enter')
     if process_running("MuAwaY"):
         return True
-    else:
-        print("Cannot Open App!")
-        return False
+    print("Cannot Open App!")
+    return False
 
 
 def find_item(item, wait=3):
+    """find item given its image then return its location on screen"""
     item_path = FolderPath.ITEM + item
     item_img = cv2.imread(item_path)
     item_loc = None
-    for i in range(3):
+    for _ in range(3):
         time.sleep(wait)
         item_loc = pyautogui.locateOnScreen(item_img)
-        if item_loc: break
+        if item_loc:
+            break
     if item_loc is None:
         print(item + "button not found")
         return False
@@ -43,6 +46,7 @@ def find_item(item, wait=3):
 
 
 def click_item(item: str, wait=3):
+    """click to an item given its image"""
     item_loc = find_item(item, wait)
     click(item_loc["x"], item_loc["y"])
     return True
@@ -55,25 +59,27 @@ def first_reset_farm():
 
 
 def check_screen(screen: str):
-    if screen == "start":
-        if find_item(Item.START): return True
-    if screen == "choose_server":
-        if find_item(Item.VIP): return True
-    if screen == "choose_character":
-        if find_item(Item.C_MAGIC): return True
+    """check which screen is showed"""
+    if screen == "start" and find_item(Item.START):
+        return True
+    if screen == "choose_server" and find_item(Item.VIP):
+        return True
+    if screen == "choose_character" and find_item(Item.C_MAGIC):
+        return True
     return False
 
 
 def move_character(x=1, y=1):
+    """move the character by x,y point"""
     if x > 0:
-        for i in range(x):
+        for _ in range(x):
             click(814, 468)
     if x < 0:
-        for i in range(abs(x)):
+        for _ in range(abs(x)):
             click(646, 362)
     if y > 0:
-        for i in range(x):
+        for _ in range(x):
             click(803, 348)
     if y < 0:
-        for i in range(abs(x)):
+        for _ in range(abs(x)):
             click(642, 643)
