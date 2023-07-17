4661b34288578994426d72011b837bec7eebc6bb
VitaHoang <hvhung.oc@gmail.com>
2023-07-17 21:54:34 +0700

    update README.md

--- final/test.py
+++ final/test.py
@@ -1,4 +1,7 @@
-from control import check_disconnected
+from control import check_disconnected, train_after_reset
+from models.character import Character
 
 check_disconnected()
+character = Character()
+train_after_reset(character)
 
